ae5b0956b48b62b99009440978f7e46b108ff78c
vagrant <vagrant@packager>
2014-01-30 06:47:21 +0000

    inital commit

---
+++ config/projects/prm.rb
@@ -0,0 +1,38 @@
+#
+# Copyright:: Copyright (c) 2014 Ryan Langford.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "prm"
+maintainer "rlangford@gmail.com"
+homepage "http://ryanlangford.com"
+
+replaces        "prm"
+install_path    "/opt/prm"
+build_version   Omnibus::BuildVersion.new.semver
+build_iteration 1
+
+# creates required build directories
+dependency "preparation"
+
+# version manifest file
+dependency "version-manifest"
+
+# prm dependencies/components
+dependency "prm"
+
+exclude "\.git*"
+exclude "bundler\/git"
+
